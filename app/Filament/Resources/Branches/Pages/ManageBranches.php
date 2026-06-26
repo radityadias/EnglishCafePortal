66ad1ab5be97262b2bc06702c77f2e66b1b7fa79
radityadias <radityadias24@gmail.com>
2026-06-26 16:57:25 +0700

    add geofence service

--- app/Filament/Resources/Branches/Pages/ManageBranches.php
+++ app/Filament/Resources/Branches/Pages/ManageBranches.php
@@ -13,7 +13,8 @@ class ManageBranches extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()
+            ->label('Branch Baru'),
         ];
     }
 }
---
+++ app/Services/GeofenceService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+class GeofenceService
+{
+    private const EARTH_RADIUS_METER = 6371000;
+
+    private static function calculateDelta(float $point1, float $point2)
+    {
+        return deg2rad($point2 - $point1);
+    }
+
+    private static function calculateChordLength(float $lat1, float $lon1, float $lat2, float $lon2, float $latDelta, float $lonDelta)
+    {
+        return
+            sin($latDelta / 2) * sin($latDelta / 2) +
+            cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
+            sin($lonDelta / 2) * sin($latDelta / 2);
+    }
+
+    private static function calculateAngularDistance(float $a)
+    {
+        return 2 * atan2(sqrt($a), sqrt(1 - $a));
+    }
+
+    public function calculateDistance(float $lat1, float $lon1, float $lat2, float $lon2)
+    {
+        $earthRadius = self::EARTH_RADIUS_METER;
+        $latDelta = $this->calculateDelta($lat1, $lat2);
+        $lonDelta = $this->calculateDelta($lon1, $lon2);
+        $a = $this->calculateChordLength($lat1, $lon1, $lat2, $lon2, $latDelta, $lonDelta);
+        $c = $this->calculateAngularDistance($a);
+
+        return $earthRadius * $c;
+    }
+}
