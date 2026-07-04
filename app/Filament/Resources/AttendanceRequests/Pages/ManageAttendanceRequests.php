d4df162fba442333415d09a8df023bbdb6b7dc26
Fafa Alfarizi <alfarizifafa14@gmail.com>
2026-07-04 15:26:51 +0700

    update and create attendance request

---
+++ app/Filament/Resources/AttendanceRequests/Pages/ManageAttendanceRequests.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\AttendanceRequests\Pages;
+
+use App\Filament\Resources\AttendanceRequests\AttendanceRequestResource;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageAttendanceRequests extends ManageRecords
+{
+    protected static string $resource = AttendanceRequestResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
+}
