6bca5423202824d7f084382045eeda603c0b4ab7
radityadias <radityadias24@gmail.com>
2026-07-12 16:21:00 +0700

    add import feature for user

--- app/Filament/Resources/Users/Pages/ManageUsers.php
+++ app/Filament/Resources/Users/Pages/ManageUsers.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\Users\Pages;
 
+use App\Filament\Imports\UserImporter;
 use App\Filament\Resources\Users\UserResource;
 use Filament\Actions\CreateAction;
+use Filament\Actions\ImportAction;
 use Filament\Resources\Pages\ManageRecords;
+use Filament\Support\Icons\Heroicon;
 
 class ManageUsers extends ManageRecords
 {
@@ -14,7 +17,12 @@ class ManageUsers extends ManageRecords
     {
         return [
             CreateAction::make()
+                ->icon(Heroicon::Plus)
                 ->label('Tambah User'),
+            ImportAction::make()
+                ->label('Import User')
+                ->icon(Heroicon::ArrowUpTray)
+                ->importer(UserImporter::class)
         ];
     }
 }
