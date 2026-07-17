8c1829c65cff619dcdfcad8de6e81d85092cfa9f
Fafa Alfarizi <alfarizifafa14@gmail.com>
2026-07-17 15:44:19 +0700

    update user seeder and user resource

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -41,6 +41,12 @@ class UserResource extends Resource
                     ->label('Alamat Email')
                     ->email()
                     ->required(),
+                TextInput::make('password')
+                    ->label('Kata Sandi')
+                    ->password()
+                    ->required()
+                    ->dehydrateStateUsing(fn ($state) => filled($state) ? bcrypt($state) : null)
+                    ->dehydrated(fn ($state) => filled($state)),
                 TextInput::make('phone')
                     ->label('Nomor Telepon'),
                 Select::make('position')
